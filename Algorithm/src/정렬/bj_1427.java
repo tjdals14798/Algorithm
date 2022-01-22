3c1f4826f07475d0b47e096402ea6b3c1117d465
tjdals14798 <tjdals14798@naver.com>
2022-01-22 15:38:05 +0900

    -0122

---
+++ Algorithm/src/정렬/bj_1427.java
@@ -0,0 +1,21 @@
+package Á¤·Ä;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class bj_1427 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char arr[] = sc.nextLine().toCharArray();
+		
+		Arrays.sort(arr);
+		
+		for(int i = arr.length-1;i>=0;i--) {
+			System.out.print(arr[i]);
+		}
+	}
+
+}
