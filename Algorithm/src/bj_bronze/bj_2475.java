26fffb7f2987a6de7e1b79fa4ab29d882eaf936b
tjdals14798 <tjdals14798@naver.com>
2022-04-09 18:20:01 +0900

    -0409

---
+++ Algorithm/src/bj_bronze/bj_2475.java
@@ -0,0 +1,23 @@
+package bj_bronze;
+
+import java.util.Scanner;
+
+public class bj_2475 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int arr[]=new int[5];
+		int sum = 0;
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+
+		for(int i=0;i<arr.length;i++) {
+			sum+=arr[i]*arr[i];
+		}
+		
+		System.out.println(sum%10);
+		
+	}
+
+}
