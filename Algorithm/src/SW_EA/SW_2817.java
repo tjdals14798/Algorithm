50e56b95de160621a3f0c0931e77e017686fd1b8
tjdals14798 <tjdals14798@naver.com>
2022-04-26 13:24:57 +0900

    -0426

---
+++ Algorithm/src/SW_EA/SW_2817.java
@@ -0,0 +1,43 @@
+package SW_EA;
+
+import java.util.Scanner;
+
+public class SW_2817 {
+	
+	static int N,K;
+	static int arr[];
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int tc = 1;tc <= T;tc++) {
+			N = sc.nextInt();
+			K=sc.nextInt();
+			
+			arr = new int[N];
+			for(int i = 0;i < N;i++) arr[i]= sc.nextInt();
+			
+			solve(0,0);
+			
+			System.out.printf("#%d %d\n",tc,res);
+			
+		}
+
+	}
+	
+	static int res;
+	private static void solve(int idx,int sum)	{
+		if(sum==K) {
+			res++;
+			return;
+		}
+		
+		if(sum>K || idx>N-1) return;
+		solve(idx+1, sum+arr[idx]);
+		solve(idx+1, sum);
+		
+		
+	}
+
+}
