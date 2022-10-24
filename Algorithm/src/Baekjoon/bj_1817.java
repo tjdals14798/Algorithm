c4f6cd882c3c91a7658b234c295d7dc70ac5bd00
성민 <tjdals14798@naver.com>
2022-10-24 13:24:01 +0900

    10/24

---
+++ Algorithm/src/Baekjoon/bj_1817.java
@@ -0,0 +1,41 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_1817 {
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int arr[] = new int[n];
+		
+		if(n == 0) {
+			System.out.println(0);
+			return;
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		int box = 1, sum = 0;
+		
+		for(int i = 0; i < n; i++) {
+			sum += arr[i];
+			if(sum > m) {
+				box++;
+				sum = arr[i];
+			}
+		}
+		System.out.println(box);
+
+	}
+
+}
