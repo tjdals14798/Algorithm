bca276d95aaf85677182da7b32714988c55dd8b4
성민 <tjdals14798@naver.com>
2023-07-27 22:11:20 +0900

    07/27

---
+++ Algorithm/src/Baekjoon/bj_10546.java
@@ -0,0 +1,38 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class bj_10546 {
+
+	public static void main(String[] args)throws IOException {
+		  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        int n = Integer.parseInt(br.readLine());
+
+	        Map<String, Integer> map = new HashMap<>();
+	        for(int i = 0; i < n; i++) {
+	            String key = br.readLine();
+	            if(map.get(key) == null) {
+	                map.put(key, 1);
+	            }else {
+	                map.put(key, map.get(key) + 1);
+	            }
+	        }
+
+	        for(int i = 0; i < n-1; i++) {
+	            String key = br.readLine();
+	            map.put(key, map.get(key) - 1);
+	        }
+
+	        for(String key : map.keySet()) {
+	            if(map.get(key) != 0) {
+	                System.out.println(key);
+	                break;
+	            }
+	        }
+	    }
+	}
